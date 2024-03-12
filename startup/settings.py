bba370f49d77f10c8a38c109ce8f512ab59d5f33
hallotechofficial <163123829+hallotechofficial@users.noreply.github.com>
2024-03-12 17:19:03 +0530

    .

--- startup/settings.py
+++ startup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-xczm*5!owas4a=pvscfzskfmyz8kzls=iophp%l4#1vi6%+cbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
