cc5026e730fbd39f6583085d3d100fcc16867561
hallotechofficial <163123829+hallotechofficial@users.noreply.github.com>
2024-03-13 17:49:29 +0530

    n

--- app1/views.py
+++ app1/views.py
@@ -34,11 +34,6 @@ def about(request):
         msg.send()
     return render(request,'about.html')
 
-def feature(request): 
-    return render(request,'feature.html')
-def blog(request): 
-    return render(request,'blog.html')
-
 def contact(request): 
     if request.method == "POST":
         name = request.POST.get('name')
@@ -46,10 +41,8 @@ def contact(request):
         subject = request.POST.get('subject')
         message = request.POST.get('message')
         
-        # Construct the email body to include name, email, subject, and message
         email_body = f"Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}"
 
-        # Send the email
         msg = EmailMultiAlternatives(
             subject=subject,
             body=email_body,
@@ -58,15 +51,8 @@ def contact(request):
         )
         msg.send()
 
-    # Regardless of whether the form was submitted or not, render the contact.html template
     return render(request, 'contact.html')
 
-def detail(request): 
-    return render(request,'detail.html')
-def price(request): 
-    return render(request,'price.html')
-def quote(request): 
-    return render(request,'quote.html')
 
 def service(request): 
     if request.method == "POST":
@@ -75,10 +61,8 @@ def service(request):
         service = request.POST.get('service')
         message = request.POST.get('message')
         
-        # Construct the email body to include name, email, subject, and message
         email_body = f"Name: {name}\nEmail: {email}\nService: {service}\nMessage: {message}"
 
-        # Create EmailMultiAlternatives instance
         msg = EmailMultiAlternatives(
             subject='Service Request',  # Set the subject here
             body=email_body,
@@ -86,25 +70,9 @@ def service(request):
             to=['hallotechofficial@gmail.com']
         )
 
-        # Send the email
         msg.send()
 
     return render(request, 'service.html')
-
-def team(request): 
-    return render(request,'team.html')
-def testimonial(request): 
-    return render(request,'testimonial.html')
-def web_price(request): 
-    return render(request,'w_price.html')
-def iot_price(request): 
-    return render(request,'iot_price.html')
-def ai_price(request): 
-    return render(request,'ai_price.html')
-def hardware_price(request): 
-    return render(request,'h_price.html')
-def media_price(request): 
-    return render(request,'m_price.html')
 
 def project(request):
     Posts = Post.objects.all()
