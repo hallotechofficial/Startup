7186ff64a8a141c021d0e81cecde713497e04418
hallotechofficial <163123829+hallotechofficial@users.noreply.github.com>
2024-03-13 19:36:37 +0530

    a

--- startup/settings.py
+++ startup/settings.py
@@ -80,13 +80,9 @@ WSGI_APPLICATION = "startup.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'zfArmGDpNETQEpGkhszwKXCCehvOJqlG',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '16468',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
