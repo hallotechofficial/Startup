24056c3b640720b9f7ccf098210aa97b0976e4f8
hallotechofficial <163123829+hallotechofficial@users.noreply.github.com>
2024-03-16 11:16:40 +0530

    Hi

--- startup/settings.py
+++ startup/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEM_DIRS= Path(__file__).joinpath(BASE_DIR,'templates')
-STA_DIRS= Path(__file__).joinpath(BASE_DIR,'staticfiles_build')
+STA_DIRS= Path(__file__).joinpath(BASE_DIR,'staticf')
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     STA_DIRS,
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles_builds'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
