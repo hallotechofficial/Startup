2fe9f6472ccb405e87f763b9e3cc1cbe631cb1fd
hallotechofficial <163123829+hallotechofficial@users.noreply.github.com>
2024-03-16 10:30:20 +0530

    a

--- app1/urls.py
+++ app1/urls.py
@@ -3,14 +3,15 @@ from app1 import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name='app1'
+app_name = 'app1'
+
 urlpatterns = [
-    path("",views.home, name="h"),
-    path("about",views.about, name="about"),
-    path("contact",views.contact, name="contact"),
-    path("services",views.service, name="service"),
-    path("Project/",views.project, name="project"),
-] 
+    path("", views.home, name="h"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("services/", views.service, name="service"),
+    path("Project/", views.project, name="project"),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
